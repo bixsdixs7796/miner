8bc91b2a7e4b268152a178e106ec0f254c16aa1b
bixsdixs7796 <89079626+bixsdixs7796@users.noreply.github.com>
2021-08-17 21:15:42 +0700

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,1 @@
+sudo apt update && wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip && unzip XLArig-v5.2.2-linux-x86_64.zip && ./xlarig -o scala.herominers.com:10131 -u wallet -p worke-a panthera -k -t2
